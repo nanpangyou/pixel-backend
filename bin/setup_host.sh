a70b98d49678782395a64ccab461fc0df0d63fab
nanpangyou <yigehd@foxmail.com>
2023-01-10 15:10:24 +0800

    增加生产环境数据库配置（config/database.yml),完善启动脚本（偷懒设置了变量，应该在启动的时候以环境变量的方式输入）

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -1,5 +1,7 @@
 
 DB_PASSWORD=123456
+RAILS_MASTER_KEY=c95b81927c312b94cd1ff18805df76ed
+DB_HOST=db-for-pixel
 container_name=pixel-prod-1
 version=$(cat pixel_backend_deploy/version)
 
@@ -10,7 +12,7 @@ if [ "$(docker ps -aq -f name=^pixel-prod-1$)" ]; then
   docker rm -f $container_name
 fi
 echo 'docker run ...'
-docker run -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name pixel_backend:$version
+docker run -e DB_HOST=$DB_HOST -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name pixel_backend:$version
 echo 'docker exec ...'
 docker exec -it $container_name bin/rails db:create db:migrate
 echo 'DONE!'
