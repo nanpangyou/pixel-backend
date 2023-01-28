cb7a3c35b0e0b96422a48c0e33981f7017a8859f
nanpangyou <yigehd@foxmail.com>
2023-01-28 14:39:47 +0800

    验证码发送需要间隔1分钟

--- spec/requests/validation_codes_spec.rb
+++ spec/requests/validation_codes_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe "ValidationCodes", type: :request do
     it "可以生成验证码" do
       post "/api/v1/validation_codes", params: { email: "yigehd@foxmail.com" }
       expect(response).to have_http_status(200)
+      post "/api/v1/validation_codes", params: { email: "yigehd@foxmail.com" }
+      expect(response).to have_http_status(429)
     end
   end
 end
